c98663e478f7c93f67fc7b11905fb2eaaf90c24b
Kayla Cammack <kaylacammack@gmail.com>
2022-12-12 07:31:54 -0700

    Got basic layout done

---
+++ src/components/ProfileSnapshot.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            <div class="card mb-3">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+                        This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        setup() {
+            return {
+
+            };
+        },
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
